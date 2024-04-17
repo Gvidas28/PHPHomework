f0569fd615d4923ffdab18cd40826cce4263c664
Gvidas Lukoševičius <52102855+Gvidas28@users.noreply.github.com>
2024-04-17 22:00:26 +0300

    (TASK-3) Login & Register pages (#3)
    Co-authored-by: Gvidas28 <gvidasluk28@gmail.com>

--- homework/react/src/views/Register.jsx
+++ homework/react/src/views/Register.jsx
@@ -1,1 +1,29 @@
-export default function Register() {}
+import { Link } from "react-router-dom";
+
+export default function Register() {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">PHP Homework - Conferences</h1>
+                    <input placeholder="Username"></input>
+                    <input type="email" placeholder="Email"></input>
+                    <input type="password" placeholder="Password"></input>
+                    <input
+                        type="password"
+                        placeholder="Repeat Password"
+                    ></input>
+                    <button className="btn btn-block">Register</button>
+                    <p className="message">
+                        Already registered?
+                        <Link to="/login"> Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+}
